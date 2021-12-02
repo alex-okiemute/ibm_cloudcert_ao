3f5b8e31d0b2b1353a37cd1a74e6379efa13548b
Alex Onojeghuo <lexisgis@yahoo.com>
2021-12-02 20:14:42 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    //Create input variables
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value; //variable rate
+    var years = document.getElementById("years").value; //variable years
+    var interest = principal * years * rate /100; //variable interest
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>" 
 }
-        
+
+//Function that reads the value of the range slider and displays dynatically
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+//Function to ensure Principal values are not 0 or negative
+function checkdata(){
+    //create references to the input elements we wish to validate
+    var principalval = document.getElementById("principal");
+    
+    if (principalval.value <= 0){
+        alert("Enter a positive value");
+        principalval.focus();
+        return false;
+    }
+    return true;    
+}
+
